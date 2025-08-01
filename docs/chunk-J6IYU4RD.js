729d2e3660b0f7b1439814be28fb6e0ac3f8d209
Fook Won <51104303+fookwon@users.noreply.github.com>
2025-08-01 20:57:48 +0800

    updated

---
+++ docs/chunk-J6IYU4RD.js
@@ -0,0 +1,1 @@
+import{h as m,ja as o}from"./chunk-KEUU4GSS.js";var g=m((t,e)=>{let n;return e==="sandbox"&&(n=o("#i"+t)),(e==="sandbox"?o(n.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{g as a};
